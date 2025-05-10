b36f3645331185690ed4c8c5f51233c2c144c2cb
Ivan <ivan-kozlow04@mail.ru>
2025-05-10 15:50:45 +0300

    update for run deploy

--- src/description/description.module.ts
+++ src/description/description.module.ts
@@ -6,7 +6,7 @@ import { DescriptionService } from './description.service'
 import { DescriptionController } from './description.controller'
 
 @Module({
-	controllers: [DescriptionController],
 	providers: [PrismaService, DescriptionService],
+	controllers: [DescriptionController],
 })
 export class DescriptionModule {}
