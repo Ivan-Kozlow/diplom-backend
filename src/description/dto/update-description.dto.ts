d3398c97a7c8e861d73aa66b8aafdddc04b02922
Ivan <ivan-kozlow04@mail.ru>
2025-06-20 19:01:10 +0300

    add fields for update book

--- src/description/dto/update-description.dto.ts
+++ src/description/dto/update-description.dto.ts
@@ -2,5 +2,11 @@ export class UpdateDescriptionDto {
 	recipient?: string
 	checkout_date?: string
 
+	year_created?: string
+	publisher?: string
+	book_name?: string
+	book_genre?: string
+	author?: string
+
 	uid: string
 }
